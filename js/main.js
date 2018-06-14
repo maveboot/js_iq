4007c4508313fdf2b1b9a6fa2c11eed7175ff439
maveboot <me_aint_no_wanksta@hotmail.com>
2018-06-15 01:55:27 +0200

    fixed CSS removed html snippets

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// index.html startButton
+var startButton = document.querySelector('.button_box');
+
+// question.html nextButton
+var nextButton = document.querySelector('#button_next');
+
+// Redirect to question.html
+startButton.onclick = function () {
+	location = 'question.html';
+};
+
+// Go to next question
+nextButton.onclick = function () {
+
+}
+
